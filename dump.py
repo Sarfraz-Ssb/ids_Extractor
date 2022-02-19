11e08b2a4d34b10fb46f89605b7250be3943034c
Sarfraz-Baloch <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-02-19 18:03:54 +0500

    Update dump.py

--- dump.py
+++ dump.py
@@ -7,7 +7,7 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from Sarfraz1 import bnsbuy
+    from Sarfraz import bnsbuy
     bnsbuy()
 elif bit == '32bit':
     print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
